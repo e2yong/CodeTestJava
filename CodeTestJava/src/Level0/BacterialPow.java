5108989aed5ab7d5c17811f6b4ba940150376d9c
yong <e2yong98@gmail.com>
2024-12-07 15:19:42 +0900

    세균 증식

---
+++ CodeTestJava/src/Level0/BacterialPow.java
@@ -0,0 +1,40 @@
+package Level0;
+
+/**
+ * 세균 증식
+ * 거듭제곱
+ */
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class BacterialPow {
+
+    static int n, t;
+    static int answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
+
+        answer = solution(n, t);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+    }
+
+    public static int solution(int n, int t) {
+
+        answer = n * (int) Math.pow(2, t);
+        answer = n << t;
+
+        return answer;
+    }
+
+}
